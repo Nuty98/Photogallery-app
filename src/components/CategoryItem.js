26a3b2b3bfe4412a22ed1fa5ce1c107b788b4063
Nuty98 <martin.knutelsky@gmail.com>
2018-03-21 21:02:32 +0100

    edit CategoryItem component

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const GalleryItem = ({ category }) => {
+const CategoryItem = ({ category, imagePath }) => {
   return (
     <div className="col-lg-3 col-md-4 col-sm-6 ">
       <Link
-        to={`/gallery/${category.name}`}
+        to={`/gallery/${category}`}
         className="link"
         style={{ textDecoration: 'none' }}
       >
         <div className="item">
           <div className="thumbnail">
-            <img src={category.url} />
+            <img
+              src={`http://api.programator.sk/images/500x500/${imagePath}`}
+            />
           </div>
-          <div className="categoryName">{category.name.toUpperCase()}</div>
+          <div className="categoryName">{category.toUpperCase()}</div>
         </div>
       </Link>
     </div>
   );
 };
-export default GalleryItem;
+export default CategoryItem;
