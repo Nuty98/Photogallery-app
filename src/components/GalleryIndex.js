f6f7ad72586aefdcdeeb26983a01d2e9fea2dead
Nuty98 <martin.knutelsky@gmail.com>
2018-03-22 00:11:42 +0100

    edit 'GalleryIndex' component - refactor return  statement

--- src/components/GalleryIndex.js
+++ src/components/GalleryIndex.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import GalleryList from './GalleryList';
 
 const photoList = [
@@ -10,29 +9,22 @@ class GalleryIndex extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
+    this.state = {
       galleries: [],
-    }
+    };
   }
 
   componentDidMount() {
-    const url = "http://api.programator.sk/gallery";
+    const url = 'http://api.programator.sk/gallery';
     fetch(url)
-    .then(res => res.json())
-    .then( data => {
-      this.setState({ galleries: data.galleries })
-    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ galleries: data.galleries });
+      });
   }
 
   render() {
-    // console.log(this.state.galleries);
-    return (
-      <div>
-        <div className="container">
-          <GalleryList galleries={this.state.galleries} type={'index'} />
-        </div>
-      </div>
-    );
+    return <GalleryList galleries={this.state.galleries} type="index" />;
   }
 }
 
