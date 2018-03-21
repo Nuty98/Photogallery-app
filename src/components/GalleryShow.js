a9555b80eae2c7ee5199864dac298d2ba2849440
Nuty98 <martin.knutelsky@gmail.com>
2018-03-22 00:12:42 +0100

    edit 'GalleryShow' component - add ajax call to API

--- src/components/GalleryShow.js
+++ src/components/GalleryShow.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
-
+import GalleryList from './GalleryList';
+import NotFound from './NotFound';
 
 class GalleryShow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      error: null,
+      images: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = `http://api.programator.sk/gallery/${this.props.match.params.id}`;
+    fetch(url)
+      .then( res => res.json())
+      .then(
+        data => {
+          // console.log(data);
+          this.setState({
+            isLoaded: true,
+            images: data.images,
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
+  }
+
   render() {
-    console.log(this.props)
-    return <div> Gallery Show Page of {this.props.match.params.id}</div>
+    const { error, isLoaded, images} = this.state;
+    console.log(error);
+    if (error) {
+      return <NotFound />
+    } else if ( !isLoaded) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <GalleryList
+        images={images}
+        type="show"
+        category={this.props.match.params.id}
+      />
+    );
   }
 }
 
