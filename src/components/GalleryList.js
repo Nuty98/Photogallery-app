5d718ce3086fbcdcc5120d83b4da412dcf3a8c4a
Nuty98 <martin.knutelsky@gmail.com>
2018-03-22 00:14:30 +0100

    edit 'GalleryList' component - change 'generateItemList' method

--- src/components/GalleryList.js
+++ src/components/GalleryList.js
@@ -2,39 +2,30 @@ import React, { Component } from 'react';
 import GalleryItem from './GalleryItem';
 import CategoryItem from './CategoryItem';
 
-
 class GalleryList extends Component {
-
   generateItemList = () => {
-    // if(this.props.type == "index"){
-    //   return this.props.galleries.map( (category, i) => {
-    //     return (
-    //       <CategoryItem key={i} category={category} /> 
-    //     );
-    //   });
-    // } else {
-    //   return this.props.gallery.map( (picture, i) => {
-    //     return <GalleryItem key={i} url={picture.url} />
-    //   });
-    // }
-
-    if(this.props.type == "index") {
-      return this.props.galleries.map( (gallery, i) => {
-        if(typeof(gallery.image) !== 'undefined')
-          return <CategoryItem key={i} category={gallery.path} imagePath={gallery.image.fullpath}/> 
-          // console.log(gallery.image.fullpath);
+    if (this.props.type == 'index') {
+      return this.props.galleries.map((gallery, i) => {
+        if (typeof gallery.image !== 'undefined') {
+          return (
+            <CategoryItem
+              key={i}
+              category={gallery.path}
+              imagePath={gallery.image.fullpath}
+            />
+          );
+        }
+      });
+    } else {
+      return this.props.images.map((image, i) => {
+        return <GalleryItem key={i} imagePath={image.fullpath} />;
       });
     }
-  }
-  
+  };
+
   render() {
-    console.log(this.props.galleries);
     const items = this.generateItemList();
-    return (
-      <div className="row text-center">
-        {items}
-      </div>
-    );
+    return <div className="row text-center">{items}</div>;
   }
 }
 
