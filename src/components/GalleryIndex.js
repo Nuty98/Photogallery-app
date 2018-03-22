0352e6651b11c24f92785b7089371cbf6036007f
Nuty98 <martin.knutelsky@gmail.com>
2018-03-22 19:23:16 +0100

    edit component 'GalleryIndex' - add error handling in ajax call

--- src/components/GalleryIndex.js
+++ src/components/GalleryIndex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GalleryList from './GalleryList';
+import UndefinedError from './UndefinedError';
 
 const photoList = [
   // require("../photos/food-salad-healthy-lunch.jpg"),
@@ -10,6 +11,8 @@ class GalleryIndex extends Component {
     super(props);
 
     this.state = {
+      isLoaded: false,
+      error: null,
       galleries: [],
     };
   }
@@ -17,14 +20,35 @@ class GalleryIndex extends Component {
   componentDidMount() {
     const url = 'http://api.programator.sk/gallery';
     fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ galleries: data.galleries });
-      });
+      .then(res => {
+        if (res.status !== 200) 
+          throw new Error(res.status);
+        return res.json();
+      })
+      .then(
+        data => {
+          this.setState({
+            isLoaded: true,
+            galleries: data.galleries,
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
   }
 
   render() {
-    return <GalleryList galleries={this.state.galleries} type="index" />;
+    const { isLoaded, error, galleries } = this.state;
+    if (error) {
+      return <UndefinedError />;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    return <GalleryList galleries={galleries} type="index" />;
   }
 }
 
