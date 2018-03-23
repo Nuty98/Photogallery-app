6dc7e894e62696f9d47908be6c40950b91004464
Nuty98 <martin.knutelsky@gmail.com>
2018-03-23 02:33:05 +0100

    add 'AddCategory' component

---
+++ src/components/AddCategory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AddCategory extends Component {
+  render() {
+    return <div className="col-lg-3 col-md-4 col-sm-6">
+              Add category component!
+          </div>
+  }
+
+}
+
+export default AddCategory;
