34542f8e06fd984291c80dc7c0011ccceb419f19
Nuty98 <martin.knutelsky@gmail.com>
2018-03-23 02:01:57 +0100

    edit 'GalleryItem' component - add classes 'gallery-item' and 'img-container'

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 const GalleryItem = ({ imagePath }) => {
   return (
-    <div className="col-lg-3 col-md-4 col-sm-6 ">
-      <div className="thumbnail">
-        <img src={`http://api.programator.sk/images/0x0/${imagePath}`} />
+    <div className=" gallery-item col-lg-3 col-md-4 col-sm-6">
+      <div className="img-container">
+        <img alt="" src={`http://api.programator.sk/images/0x0/${imagePath}`} />
       </div>
     </div>
   );
