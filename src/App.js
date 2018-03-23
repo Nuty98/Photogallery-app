90c1bebd1832ee5835891cb34e76c64027fa0e23
Nuty98 <martin.knutelsky@gmail.com>
2018-03-23 01:59:08 +0100

    edit App.js - returns only 'Routes' component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import Routes from './components/Routes';
 import './App.css'
 
-import GalleryIndex from './components/GalleryIndex';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
         <Routes />
-      </div>
     );
   }
 }
