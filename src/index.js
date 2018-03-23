29bfd122bfccea79dd41d5dfea0f05555417803f
Nuty98 <martin.knutelsky@gmail.com>
2018-03-23 01:03:13 +0100

    edit index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
