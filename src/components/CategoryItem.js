210c50c62bc375e6b41382d24b7980abd67e713a
Nuty98 <martin.knutelsky@gmail.com>
2018-03-23 01:02:20 +0100

    edit CategoryItem - add case when image of category does not exists

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const CategoryItem = ({ category, imagePath }) => {
+const CategoryItem = ({ category, imagePath, empty }) => {
+  let url;
+  if(! empty) {
+    url = `http://api.programator.sk/images/0x0/${imagePath}`;
+  } else {
+    url = require("../photos/placeholder_image.gif");
+  }
+
   return (
-    <div className="col-lg-3 col-md-4 col-sm-6 ">
+    <div className="col-lg-3 col-md-4 col-sm-6">
       <Link
         to={`/gallery/${category}`}
         className="link"
         style={{ textDecoration: 'none' }}
       >
         <div className="item">
-          <div className="thumbnail">
-            <img 
-              src={`http://api.programator.sk/images/0x0/${imagePath}`} 
-              />
+          <div className="img-container">
+            <img
+              alt=""
+              src={url}
+            />
           </div>
-          <div className="categoryName">{category.toUpperCase()}</div>
+          <div className="category-name">{category.toUpperCase()}</div>
         </div>
       </Link>
     </div>
