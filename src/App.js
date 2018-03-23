5b251a99b74db75ce80c0c4d0019aa08941ef8cb
Nuty98 <martin.knutelsky@gmail.com>
2018-03-23 16:17:46 +0100

    edit App.js and AddCategory.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import Routes from './components/Routes';
-import './App.css'
-
+import './App.css';
 
 class App extends Component {
   render() {
-    return (
-        <Routes />
-    );
+    return <Routes />;
   }
 }
 
