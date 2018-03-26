393b6c087f88cb8efa9c463f6511f06a0a92ae8f
Nuty98 <martin.knutelsky@gmail.com>
2018-03-27 01:01:46 +0200

    edit 'AddImage.js' component - add styling, functionality is not done yet - problems with fakepath

--- src/components/AddImage/AddImage.js
+++ src/components/AddImage/AddImage.js
@@ -1,8 +1,9 @@
-import styles from './AddImage.css'
 import React, { Component } from 'react';
-
-import { Grid, Modal, Header } from 'semantic-ui-react';
+import styles from './AddImage.css';
+import { Grid, Modal, Header, Button, Icon } from 'semantic-ui-react';
 import ModalContent from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
+
 
 // eslint-disable-next-line
 class AddImage extends Component {
@@ -10,29 +11,67 @@ class AddImage extends Component {
     super(props);
 
     this.state = {
-      showModal: false
-    }
+      showModal: false,
+      imagesToAdd: []
+    };
+  }
+
+  handleChange = e => {
+    // console.log(this.state.imagesToAdd);
+    let imagesToAddCpy = this.state.imagesToAdd;
+    imagesToAddCpy.push(e.target.value);
+    this.setState({imagesToAdd: imagesToAddCpy});
+    console.log(this.state.imagesToAdd);
+    e.preventDefault();
   }
 
   handleSubmit = e => {
-    
-    e.prevetDefault;
+
+    e.preventDefault();
+  };
+
+  onImageDrop = (e) => {
+    console.log("DROPPED!");
   }
 
+
   render() {
+    const url = "http://api.programator.sk/gallery/";
+    const { category } = this.props;
     return (
       <Grid.Column mobile={16} tablet={8} computer={4}>
-        
-        <Modal trigger={<div className="add-image"> Pridať fotku </div>}
+        <Modal
+          trigger={ 
+                    <div className="add-image"> 
+                      <Icon name="photo" size="big"/>             
+                      <strong>PRIDAŤ FOTKY</strong> 
+                    </div>
+                  }
           size="small"
           closeIcon
         >
           <Header>PRIDAŤ FOTKY</Header>
           <Modal.Content>
-            <form>
-              <div className="add-image-input">
-                <input type="file" />
+            <form onSubmit={this.handleSubmit} >
+              <div className="add-image-form">
+                <Dropzone
+                  multiple={true}
+                  accept="image/*"
+                  onDrop={this.onImageDrop}
+                  className="dropzone"
+                >
+                  <div><strong>SEM PRESUŇTE FOTKY</strong></div>
+                  <div>alebo</div>
+                </Dropzone>
+                <div className="file-input-image">
+                    <label id="label-image-input" htmlFor="upload-image-input">VYBERTE SÚBORY</label>
+                    <input type="file" id="upload-image-input" onChange={this.handleChange} required/>
+                </div>  
               </div>
+              
+              <div className="button-div">
+                <Button content="PRIDAŤ" icon="plus" />
+              </div>      
             </form>
           </Modal.Content>
         </Modal>
