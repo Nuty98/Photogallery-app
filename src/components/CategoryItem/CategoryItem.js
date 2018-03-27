3f5393b952e569a04b5d264ad45acb359f369c22
Nuty98 <martin.knutelsky@gmail.com>
2018-03-27 08:47:26 +0200

    edit 'CategoryItem' component - change it to the functional component and add proptypes checking

--- src/components/CategoryItem/CategoryItem.js
+++ src/components/CategoryItem/CategoryItem.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
 import styling from './CategoryItem.css';
 
-class CategoryItem extends Component {
-  handleMouseEnter = () => {
-    if (this.props.imagePath) {
-      this.props.handleMouseEnter(this.props.imagePath);
-    }
-  };
-
-  render() {
-    const { imagePath, category } = this.props;
+const CategoryItem  = ({ category, imagePath, handleMouseEnter}) => {
     let url;
     if (imagePath) 
       url = `http://api.programator.sk/images/1024x576/${imagePath}`;
@@ -22,7 +15,7 @@ class CategoryItem extends Component {
         mobile={16}
         tablet={8}
         computer={4}
-        onMouseEnter={this.handleMouseEnter}
+        onMouseEnter={handleMouseEnter}
       >
         <Link to={`/gallery/${category}`} className="link">
           <div className="category-item">
@@ -34,6 +27,17 @@ class CategoryItem extends Component {
         </Link>
       </Grid.Column>
     );
-  }
+  
 }
+
+CategoryItem.propTypes = {
+  category: PropTypes.string.isRequired,
+  imagePath: PropTypes.string,
+  handleMouseEnter: PropTypes.func
+}
+
+CategoryItem.defaultProps = {
+  imagePath: null
+}
+
 export default CategoryItem;
