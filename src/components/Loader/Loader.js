d596644c190c3774fd439ad9c2bf2c5e49371d60
Nuty98 <martin.knutelsky@gmail.com>
2018-03-27 02:30:01 +0200

    add Loader.css file

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react';
+import styling from './Loader.css';
 
 const MyLoader = ({ category }) => (
   <div className="loader">
