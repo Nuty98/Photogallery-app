609a251e68c0306dda1c58d033a2af9b4d1f7414
Nuty98 <martin.knutelsky@gmail.com>
2018-03-27 08:08:07 +0200

    divide styling of 'TitleImage' component to separate file

--- src/components/TitleImage/TitleImage.js
+++ src/components/TitleImage/TitleImage.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import styling from './TitleImage.css';
 
 const TitleImage = ({ image }) => {
   return (
-    <div className="title-image">
-      <img src={image} alt="" />
-    </div>
-  )
-}
+    <div className="title-image" style={{ backgroundImage: `url(${image})` }} />
+  );
+};
 
 export default TitleImage;
