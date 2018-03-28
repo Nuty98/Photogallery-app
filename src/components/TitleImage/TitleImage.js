9ffd395e2e9cd728171695402648a354a40c2222
Nuty98 <martin.knutelsky@gmail.com>
2018-03-28 21:39:07 +0200

    edit 'TitleImage' component and its styling

--- src/components/TitleImage/TitleImage.js
+++ src/components/TitleImage/TitleImage.js
@@ -2,19 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TitleImage.css';
 
-const TitleImage = ({ currentImage }) => {  
+const TitleImage = ({ currentImage, currentPage }) => {
   return (
-    <div id="title-image-container">
-      <div
+      <div id="title-image-container">
+        <div
         className="title-image"
-        style={{ backgroundImage: `url(${currentImage})`}}
-      />
+        style={{ backgroundImage: `url(${currentImage})` }}
+        />
+
+      <div className="header-container">
+        <div className="header">Fotogaléria</div>
+        <div className="actual-page-name">Kategórie</div>
+        <hr id="index-page-line" />
+      </div>
     </div>
   );
 };
 
 TitleImage.propTypes = {
-  currentImage: PropTypes.string.isRequired
-}
+  currentImage: PropTypes.string.isRequired,
+};
 
 export default TitleImage;
