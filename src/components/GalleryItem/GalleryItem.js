70c493561fd92804e54371ce8c552ac52a2ba582
Nuty98 <martin.knutelsky@gmail.com>
2018-04-06 03:52:44 +0200

    edit 'GalleryItem' - change resolution

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -15,7 +15,7 @@ const GalleryItem = ({ imagePath, index, handleGalleryItemClick }) => {
         <div className="img-container">
           <img
             alt=""
-            src={`http://api.programator.sk/images/0x0/${imagePath}`}
+            src={`http://api.programator.sk/images/1024x576/${imagePath}`}
           />
         </div>
       </div>
