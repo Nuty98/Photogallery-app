7e5e56a961378416aa7cbe9afb6e39c64725375a
Nuty98 <martin.knutelsky@gmail.com>
2018-04-06 03:51:23 +0200

    edit 'NotFound' - change message and add styling

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+import Icon from '../Icon';
+import './NotFound.css';
 
 const NotFound = () => (
-  <div>
-    <h3>404 page not found</h3>
-    <p>We are sorry but the page you are looking for does not exist.</p>
+  <div className="not-found-container">
+    <div className="not-found-header">Ľutujeme, táto stránka neexistuje.</div>
+    <div className="not-found-message" style={{ fontSize: '20px' }}>
+      Ak ste zadávali adresu stránky manuálne, skontrolujte či bola zadaná
+      správne.
+    </div>
+    <div style={{ marginTop: '20px' }}>
+      <Link to="/" className="link">
+        <Button>
+          <Icon name="back_black.svg" styleProps="back-svg" />
+          <span className="not-found-back-button-message">Späť na hlavnú stránku</span>
+        </Button>
+      </Link>
+    </div>
   </div>
 );
 
